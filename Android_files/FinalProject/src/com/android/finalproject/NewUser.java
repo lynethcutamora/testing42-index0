c729f7f61fb7b8c7e9f201eac136d2713460f1c8
lynethcutamora <lyneth.cutamora@gmail.com>
2015-10-12 02:42:51 +0800

    added sign up activity for android

---
+++ Android_files/FinalProject/src/com/android/finalproject/NewUser.java
@@ -0,0 +1,27 @@
+package com.android.finalproject;
+
+import java.util.ArrayList;
+import java.util.List;
+ 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+ 
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class NewUser {
+	//progress Dialog
+	private ProgressDialog pDialog;
+	
+	JSONParser jsonParser = new JSONParser();
+	
+}
